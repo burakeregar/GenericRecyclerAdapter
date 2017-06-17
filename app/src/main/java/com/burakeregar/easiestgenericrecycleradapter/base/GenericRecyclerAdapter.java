906f01b1ab35bf680c98d33ea77131071ef45be1
Burak Eregar <burake91@gmail.com>
2017-06-17 20:57:26 +0300

    Reference bug fix.

--- app/src/main/java/com/burakeregar/easiestgenericrecycleradapter/base/GenericRecyclerAdapter.java
+++ app/src/main/java/com/burakeregar/easiestgenericrecycleradapter/base/GenericRecyclerAdapter.java
@@ -11,114 +11,106 @@ import java.util.List;
 
 public class GenericRecyclerAdapter extends RecyclerView.Adapter<GenericViewHolder> {
 
-   public List mObjectList;
-   public List mOriginalObjectList;
-   private ArrayList<GenericAdapterModel> mModels;
-   private Filter mFilter;
-   private boolean mIsFilterEnabled;
+    public List mObjectList;
+    public List mOriginalObjectList;
+    private ArrayList<GenericAdapterModel> mModels;
+    private Filter mFilter;
+    private boolean mIsFilterEnabled;
 
-   public GenericRecyclerAdapter(ArrayList<GenericAdapterModel> pModels) {
-      mModels = pModels;
-   }
+    public GenericRecyclerAdapter(ArrayList<GenericAdapterModel> pModels) {
+        mModels = pModels;
+    }
 
-   public GenericRecyclerAdapter(ArrayList<GenericAdapterModel> pModels, boolean pIsFilterEnabled) {
-      mModels = pModels;
-      mIsFilterEnabled = pIsFilterEnabled;
-   }
+    public GenericRecyclerAdapter(ArrayList<GenericAdapterModel> pModels, boolean pIsFilterEnabled) {
+        mModels = pModels;
+        mIsFilterEnabled = pIsFilterEnabled;
+    }
 
-   public GenericRecyclerAdapter(int pLayout, Class pViewHolder, Class pItemType) {
-      mModels = new ArrayList<>();
-      mModels.add(new GenericAdapterModel(pLayout, pViewHolder, pItemType));
-   }
+    public GenericRecyclerAdapter(int pLayout, Class pViewHolder, Class pItemType) {
+        mModels = new ArrayList<>();
+        mModels.add(new GenericAdapterModel(pLayout, pViewHolder, pItemType));
+    }
 
-   @Override
-   public GenericViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-      LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-      GenericViewHolder holder   = null;
-      if (viewType != -1) {
-         GenericAdapterModel lModel = mModels.get(viewType);
-         View lView  = inflater.inflate(lModel.getLayout(), parent, false);
-         try {
-            Class mClass = lModel.getViewHolder();
-            holder = (GenericViewHolder) mClass.getConstructor(View.class).newInstance(lView);
-         } catch (Exception pE) {
-            throw new RuntimeException(pE.getMessage());
-         }
-      }
-      return holder;
-   }
+    @Override
+    public GenericViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        GenericViewHolder holder = null;
+        if (viewType != -1) {
+            GenericAdapterModel lModel = mModels.get(viewType);
+            View lView = inflater.inflate(lModel.getLayout(), parent, false);
+            try {
+                Class mClass = lModel.getViewHolder();
+                holder = (GenericViewHolder) mClass.getConstructor(View.class).newInstance(lView);
+            } catch (Exception pE) {
+                throw new RuntimeException(pE.getMessage());
+            }
+        }
+        return holder;
+    }
 
-   @Override
-   public void onBindViewHolder(GenericViewHolder holder, int position) {
-         holder.bindData(mObjectList.get(position));
-   }
+    @Override
+    public void onBindViewHolder(GenericViewHolder holder, int position) {
+        holder.bindData(mObjectList.get(position));
+    }
 
-   @Override
-   public int getItemViewType(int position) {
-      Class<?> lItemClass = mObjectList.get(position).getClass();
-      for (int i = 0; i < mModels.size(); i++) {
-         if (mModels.get(i).getItemType().equals(lItemClass)) {
-            return i;
-         }
-      }
-      return -1;
-   }
+    @Override
+    public int getItemViewType(int position) {
+        Class<?> lItemClass = mObjectList.get(position).getClass();
+        for (int i = 0; i < mModels.size(); i++) {
+            if (mModels.get(i).getItemType().equals(lItemClass)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-   @Override
-   public int getItemCount() {
-      return mObjectList != null ? mObjectList.size() : 0;
-   }
+    @Override
+    public int getItemCount() {
+        return mObjectList != null ? mObjectList.size() : 0;
+    }
 
-   public void setList(List pObjectList) {
-      if (this.mObjectList == null) {
-         this.mObjectList = pObjectList;
-      } else {
-         mObjectList.clear();
-         mObjectList.addAll(pObjectList);
-      }
-      if (mIsFilterEnabled) {
-         mOriginalObjectList = pObjectList;
-      }
+    public void setList(List pObjectList) {
+        if (this.mObjectList == null) {
+            this.mObjectList = new ArrayList();
+        }
+        mObjectList.addAll(pObjectList);
+        if (mIsFilterEnabled) {
+            mOriginalObjectList = pObjectList;
+        }
+        notifyDataSetChanged();
+    }
 
-      notifyDataSetChanged();
-   }
+    public List getItems() {
+        return mObjectList;
+    }
 
-   public List getItems() {
-      return mObjectList;
-   }
+    public void addNewRows(List pObjectList) {
+        if (this.mObjectList == null) {
+            this.mObjectList = new ArrayList();
+        }
+        mObjectList.addAll(pObjectList);
+        if (mIsFilterEnabled) {
+            mOriginalObjectList = mObjectList;
+        }
+        notifyDataSetChanged();
+    }
 
-   public void addNewRows(List pObjectList) {
-      if (this.mObjectList == null) {
-         this.mObjectList = pObjectList;
-      } else {
-         mObjectList.addAll(pObjectList);
-      }
+    public void addNewRows(Object pObject) {
+        if (this.mObjectList == null) {
+            this.mObjectList = new ArrayList();
+        }
+        mObjectList.add(pObject);
+        if (mIsFilterEnabled) {
+            mOriginalObjectList = mObjectList;
+        }
+        notifyDataSetChanged();
+    }
 
-      if (mIsFilterEnabled) {
-         mOriginalObjectList = mObjectList;
-      }
+    public Filter getFilter() {
+        return mFilter;
+    }
 
-      notifyDataSetChanged();
-   }
-
-   public void addNewRows(Object pObject) {
-      if (this.mObjectList == null) {
-         this.mObjectList = new ArrayList();
-      } else {
-         mObjectList.add(pObject);
-      }
-
-      if (mIsFilterEnabled) {
-         mOriginalObjectList = mObjectList;
-      }
-
-      notifyDataSetChanged();
-   }
-   public Filter getFilter() {
-      return mFilter;
-   }
-
-   public void setFilter(Filter pFilter) {
-      mFilter = pFilter;
-   }
+    public void setFilter(Filter pFilter) {
+        mFilter = pFilter;
+    }
 }
